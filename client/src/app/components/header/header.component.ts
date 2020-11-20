633a87727383c4e6d1abc7c7aeb583b44d511fa3
David Gahnassia <davidgahnassia@Davids-MacBook-Pro.local>
2020-11-20 13:35:57 +0200

    fixed bug with header and footer

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+declare let $: any;
 
 @Component({
   selector: 'app-header',
@@ -14,11 +19,7 @@ export class HeaderComponent implements OnInit {
   selectedTab = 'home'
   
 
-  constructor(public cd: ChangeDetectorRef) { }
-
-  ngOnInit(): void {
-    this.adminMode = localStorage.getItem('adminMode') === 'true';
-  }
+  constructor(public cd: ChangeDetectorRef, public router: Router) {AOS.init(); }
 
   toggleAdminMode(e) {
     this.adminMode = e.checked;
@@ -28,6 +29,60 @@ export class HeaderComponent implements OnInit {
 
   changedTab(tab) {
     this.selectedTab = tab;
+  }
+
+  ngOnInit() {
+
+    this.adminMode = localStorage.getItem('adminMode') === 'true';
+
+    //  $(window).on('scroll', function() {
+    //  console.log("broker");
+
+    // });
+
+    // $(window).on('scroll', function() {
+    //   //alert('out reached');
+    //     if($(window).scrollTop() + $('.navbar-collapse').height() - 1726 >= $('.navbar-collapse')[0].scrollHeight) {
+    //       console.log("from broker");
+
+    //         $('.navbar-collapse').css({'position':'fixe', 'top':'0'});
+    //     } else{
+    //       $('.navbar-collapse').css({'position':'sticky', 'top':'327'});
+    //     }
+    // });
+
+    var navbar = $('.navbar');
+    var navLink = $('.nav-link')
+    var imgLogoWhite = $('#logoMenuWhite')
+    var imgLogoBlue= $('#logoMenuBlue')
+    var btnSignIn = $('.btnSignIn')
+
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() <= 400) {
+        navbar.removeClass('navbar-scroll');
+        navLink.removeClass('nav-link-scroll');
+        imgLogoWhite.removeClass('d-none');
+        imgLogoBlue.addClass('d-none')
+        imgLogoBlue.removeClass('d-block');
+        btnSignIn.removeClass('btnSignInScroll')
+
+      } else  {
+        navbar.addClass('navbar-scroll');
+        navLink.addClass('nav-link-scroll');
+        imgLogoWhite.addClass('d-none');
+        imgLogoBlue.removeClass('d-none')
+        imgLogoBlue.addClass('d-block')
+        btnSignIn.addClass('btnSignInScroll')
+      }
+    });
+  }
+
+
+
+
+  goToLinkOfPath(path) {
+    this.router.navigate([`/${path}`])
   }
 
 
