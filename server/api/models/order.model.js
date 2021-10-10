a85611a3cd9b4bdd5452b85408dd0cc505c04650
menahemGershovitz <menahemgershovitz@gmail.com>
2021-10-10 22:44:41 +0300

    paypal

--- server/api/models/order.model.js
+++ server/api/models/order.model.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const orderSchema = new Schema(
-  {
-    user: { type: String, ref: 'User', required: true },
-    products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true }],
-    status: { type: String, enum: ['NEW', 'PICKUP', 'CLEANING', 'DROPOFF'], required: true },
-    dropOffAddress: { type: String, required: true },
-    pickUpAddress: { type: String, required: true },
-    pickUpDate: { type: Date, required: true },
-    dropOffDate: { type: Date, required: true },
-    price: { type: String, required: true },
-    addressDetails: { type: String, default: '' },
-    addressDetails2: { type: String, default: '' },
-    displayedId: { type: String, required: true },
-    comment: { type: String, default: '' },
-    phoneNumber: { type: String, required: true }
-  },
+const orderSchema = new Schema({
+        user: { type: String, ref: 'User', required: true },
+        products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true }],
+        status: { type: String, enum: ['NEW', 'PICKUP', 'CLEANING', 'DROPOFF'], required: true },
+        dropOffAddress: { type: String, required: true },
+        pickUpAddress: { type: String, required: true },
+        pickUpDate: { type: Date, required: true },
+        dropOffDate: { type: Date, required: true },
+        price: { type: String, required: true },
+        addressDetails: { type: String, default: '' },
+        addressDetails2: { type: String, default: '' },
+        displayedId: { type: String, required: true },
+        comment: { type: String, default: '' },
+        phoneNumber: { type: String, required: true },
+        payment: { type: String, required: true }
+    },
 
-  {
-    timestamps: true,
-    autoCreate: true
-  }
+    {
+        timestamps: true,
+        autoCreate: true
+    }
 )
 
 const OrderModel = mongoose.model('Order', orderSchema, 'orders')
--- server/api/routes/validators/order.validator.js
+++ server/api/routes/validators/order.validator.js
@@ -1,76 +1,82 @@
 const { Joi } = require('../../../helpers/joi-validator')
 
 const createOrderValidator = Joi.object({
-  _id: Joi.string()
-    .allow('')
-    .optional(),
-  user: Joi.string()
-    .allow('')
-    .optional(),
-  products: Joi.array()
-    .items(Joi.objectId())
-    .required(),
-  status: Joi.string()
-    .valid('NEW', 'PICKUP', 'CLEANING', 'DROPOFF')
-    .required(),
-  displayedId: Joi.string(),
-  dropOffAddress: Joi.string().required(),
-  pickUpAddress: Joi.string().required(),
-  pickUpDate: Joi.date().required(),
-  dropOffDate: Joi.date().required(),
-  price: Joi.string().required(),
-  phoneNumber: Joi.string().required(),
-  comment: Joi.string()
-    .allow('')
-    .optional(),
-  addressDetails: Joi.string()
-    .allow('')
-    .optional(),
-  addressDetails2: Joi.string()
-    .allow('')
-    .optional()
+    _id: Joi.string()
+        .allow('')
+        .optional(),
+    user: Joi.string()
+        .allow('')
+        .optional(),
+    products: Joi.array()
+        .items(Joi.objectId())
+        .required(),
+    status: Joi.string()
+        .valid('NEW', 'PICKUP', 'CLEANING', 'DROPOFF')
+        .required(),
+    displayedId: Joi.string(),
+    dropOffAddress: Joi.string().required(),
+    pickUpAddress: Joi.string().required(),
+    pickUpDate: Joi.date().required(),
+    dropOffDate: Joi.date().required(),
+    price: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+    comment: Joi.string()
+        .allow('')
+        .optional(),
+    addressDetails: Joi.string()
+        .allow('')
+        .optional(),
+    addressDetails2: Joi.string()
+        .allow('')
+        .optional(),
+    payment: Joi.string()
+        .allow('')
+        .optional()
 })
 const updateOrderStatusValidator = Joi.object({
-  _id: Joi.string()
-    .allow('')
-    .optional(),
-  user: Joi.object()
-    .allow({
-      createdAt: Joi.string(),
-      email: Joi.string(),
-      fullname: Joi.string(),
-      isAdmin: Joi.boolean(),
-      updatedAt: Joi.string()
-    })
-    .optional(),
-  products: Joi.array()
-    .items(Joi.objectId())
-    .required(),
-  status: Joi.string()
-    .valid('NEW', 'PICKUP', 'CLEANING', 'DROPOFF')
-    .required(),
-  displayedId: Joi.string(),
-  dropOffAddress: Joi.string().required(),
-  pickUpAddress: Joi.string().required(),
-  pickUpDate: Joi.date().required(),
-  dropOffDate: Joi.date().required(),
-  price: Joi.string().required(),
-  phoneNumber: Joi.string().required(),
-  comment: Joi.string()
-    .allow('')
-    .optional(),
-  addressDetails: Joi.string()
-    .allow('')
-    .optional(),
-  addressDetails2: Joi.string()
-    .allow('')
-    .optional(),
-  pressing: Joi.string()
-    .allow('')
-    .optional()
+    _id: Joi.string()
+        .allow('')
+        .optional(),
+    user: Joi.object()
+        .allow({
+            createdAt: Joi.string(),
+            email: Joi.string(),
+            fullname: Joi.string(),
+            isAdmin: Joi.boolean(),
+            updatedAt: Joi.string()
+        })
+        .optional(),
+    products: Joi.array()
+        .items(Joi.objectId())
+        .required(),
+    status: Joi.string()
+        .valid('NEW', 'PICKUP', 'CLEANING', 'DROPOFF')
+        .required(),
+    displayedId: Joi.string(),
+    dropOffAddress: Joi.string().required(),
+    pickUpAddress: Joi.string().required(),
+    pickUpDate: Joi.date().required(),
+    dropOffDate: Joi.date().required(),
+    price: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+    comment: Joi.string()
+        .allow('')
+        .optional(),
+    addressDetails: Joi.string()
+        .allow('')
+        .optional(),
+    addressDetails2: Joi.string()
+        .allow('')
+        .optional(),
+    pressing: Joi.string()
+        .allow('')
+        .optional(),
+    payment: Joi.string()
+        .allow('')
+        .optional()
 })
 
 module.exports = {
-  createOrderValidator,
-  updateOrderStatusValidator
+    createOrderValidator,
+    updateOrderStatusValidator
 }
