d5a864ae35fe4a99863de4ddc68772fecd8740b1
menahemGershovitz <menahemgershovitz@gmail.com>
2021-11-20 20:28:37 +0200

    bug

--- server/lib/send-email.js
+++ server/lib/send-email.js
@@ -3,7 +3,10 @@ const nodemailer = require('nodemailer')
 const path = require('path')
 const fs = require('fs')
 const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
     service: 'Gmail',
+    port: 465,
+    secure: true,
     auth: {
         user: 'contact@cleeser.com',
         pass: 'linoyezra24'
@@ -11,10 +14,9 @@ const transporter = nodemailer.createTransport({
 })
 
 module.exports.sendWelcomeEmail = async email => {
-    console.log('sendWelcomeEmail', email);
     const template = fs.readFileSync(path.join(__dirname, 'welcome.ejs'), 'utf-8')
     await transporter.sendMail({
-        from: 'contact@cleeser.com',
+        from: 'Cleeser <contact@cleeser.com>',
         to: email,
         subject: 'Welcome to Cleeser !',
         html: ejs.render(template)
@@ -25,7 +27,7 @@ module.exports.sendOrderConfirmation = async email => {
     const template = fs.readFileSync(path.join(__dirname, 'order-confirmation.ejs'), 'utf-8')
 
     await transporter.sendMail({
-        from: 'contact@cleeser.com',
+        from: 'Cleeser <contact@cleeser.com>',
         to: email,
         subject: 'Confirmation of your order',
         html: ejs.render(template)
