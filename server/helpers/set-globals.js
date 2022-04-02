2d983e32fcfaa3d3702a219ceecd105f6275610c
menahemGershovitz <menahemgershovitz@gmail.com>
2022-04-02 22:46:17 +0300

    new article translate

--- server/helpers/set-globals.js
+++ server/helpers/set-globals.js
@@ -1,5 +1,5 @@
 module.exports.setGlobals = function() {
     global.cleeser = {
-        CATEGORIES: ['Costumes', 'Jupes', 'Manteaux', 'Pantalons', 'Robes', 'Linge de maison', 'Lavage au kilos', 'Autres']
+        CATEGORIES: ['Costume', 'Jupe', 'Manteau', 'Pantalon', 'Robe', 'Linge de maison', 'Lavage au kilos', 'Autres']
     }
 }
